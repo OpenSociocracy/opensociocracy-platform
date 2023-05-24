45df2b3d4792f0c841976ad056b5ac279ce78621
Brian Winkers <bwinkers@gmail.com>
2023-05-24 15:50:50 -0700

    Add Postgres support

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 import Fastify from "fastify";
 
 import config from "./plugins/config.js";
+import postgres from "./plugins/postgres.js";
 import auth from "./plugins/auth.js";
 import swagger from "./plugins/swagger.js";
 import indexRoutes from "./routes/index.js";
@@ -8,6 +9,7 @@ import indexRoutes from "./routes/index.js";
 export default async function appFramework() {
   const fastify = Fastify({ logger: true });
   fastify.register(config);
+  fastify.register(postgres);
   fastify.register(auth);
   fastify.register(swagger);
   fastify.register(indexRoutes);
