a1d10a9bc22e95312096528107161a0c69618daf
Brian Winkers <bwinkers@gmail.com>
2023-05-24 11:01:25 -0700

    Base app with working auth

---
+++ app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+import SuperTokens from 'supertokens-web-js';
+import Session from 'supertokens-web-js/recipe/session';
+import Passwordless from 'supertokens-web-js/recipe/passwordless'
+
+SuperTokens.init({
+    appInfo: {
+        apiDomain: "http://localhost:3000",
+        apiBasePath: "/auth",
+        appName: "OpenSociocracy",
+    },
+    recipeList: [
+        Session.init(),
+        Passwordless.init(),
+    ],
+});
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
---
+++ app/src/layouts/MainLayout.vue
@@ -0,0 +1,66 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Base App
+        </q-toolbar-title>
+
+        <div> <q-btn label="Sign In" aria-label="Sign In" @click="auth.setSignInRequired(true)" color="secondary"></q-btn> </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <PasswordlessAuthDialog
+        v-model="auth.signInRequired"
+      ></PasswordlessAuthDialog>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { defineComponent, ref } from 'vue'
+
+import { useAuthStore } from '../stores/auth';
+
+import PasswordlessAuthDialog from '../components/PasswordlessDialog.vue';
+
+
+
+    const leftDrawerOpen = ref(false)
+
+    const auth = useAuthStore();
+
+
+    const toggleLeftDrawer = () => {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      };
+
+
+</script>
