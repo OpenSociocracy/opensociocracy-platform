8fbe56fde664e8adf068e4d92fcf359f0ba44704
Brian Winkers <bwinkers@gmail.com>
2023-05-28 23:59:05 -0700

    Fixed dumps

--- docker-postgres/5-api-data-and-views-as-api.sh
+++ docker-postgres/5-api-data-and-views-as-api.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 export PGPASSWORD=$API_PASS;
-psql -v ON_ERROR_STOP=1 --username "$API_USER"  --dbname "$DB_NAME" < /sql/api-org-roles.sql
 psql -v ON_ERROR_STOP=1 --username "$API_USER"  --dbname "$DB_NAME" < /sql/reporting-schema.sql
--- docker-postgres/scripts/dump.sh
+++ docker-postgres/scripts/dump.sh
@@ -2,4 +2,4 @@
 pg_dump -s -O -h localhost -U postgres -d opensociocracy -n opensociocracy_api > ./docker-postgres/sql/api-schema.sql 
 pg_dump -s -O -h localhost -U postgres -d opensociocracy -n reporting > ./docker-postgres/sql/reporting-schema.sql
 pg_dump -s -O -h localhost -U postgres -d opensociocracy -n supertokens > ./docker-postgres/sql/supertokens-schema.sql 
-pg_dump --data-only  --column-inserts  -O -h localhost -U postgres -d opensociocracy --table opensociocracy_api.org_roles  > ./docker-postgres/sql/api-org-roles.sql 
+# pg_dump --data-only  --column-inserts  -O -h localhost -U postgres -d opensociocracy --table opensociocracy_api.org_roles  > ./docker-postgres/sql/api-org-roles.sql 
