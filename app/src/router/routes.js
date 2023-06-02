2d49e8f39e1893231e07968456fa7d9bd7f88399
Brian Winkers <bwinkers@gmail.com>
2023-06-02 09:47:57 -0700

    Start of proposal form

--- app/src/router/routes.js
+++ app/src/router/routes.js
@@ -8,6 +8,14 @@ const routes = [
     ]
   },
 
+  {
+    path: '/forms/:formName',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/FormWrapperPage.vue') }
+    ]
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
