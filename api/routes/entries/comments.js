ad2f36cf52b64b7152c8cb103b48b1d9128b42b9
Brian Winkers <bwinkers@gmail.com>
2023-06-09 21:47:56 -0700

    WiP on comments

---
+++ api/routes/entries/comments.js
@@ -0,0 +1,46 @@
+import fastifyPlugin from "fastify-plugin";
+import { verifySession } from "supertokens-node/recipe/session/framework/fastify/index.js";
+
+async function entryEntryCreateRoutes(server, options) {
+  server.get(
+    "/entries/:logbookEntryUid/comments",
+    {
+      preHandler: verifySession(),
+      schema: {
+        description: "Get a logbook entry's comments",
+        tags: ["entries"],
+        summary: "Get a logbook entry's top-level comments.",
+        response: {
+          200: {
+            description: "Success Response",
+            type: "object",
+            properties: {
+              logbookEntryUid: { type: "string" },
+              createdAt: { type: "string" },
+              updatedAt: { type: "string" },
+              note: { type: "string" },
+              pubAt: { type: "string" },
+              unPubAt: { type: "string" },
+              publicTitle: { type: "string" },
+              internalName: { type: "string" },
+              blocks: { type: "array" },
+              nuggetType: { type: "string" }
+            },
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+      const memberUid = request.session.getUserId();
+
+      const logbookEntryUid = request.params.logbookEntryUid;
+
+      const result = await server.commentService.getLogbookEntryComments(memberUid, logbookEntryUid);
+
+      return result;
+    }
+  );
+ 
+}
+
+export default fastifyPlugin(entryEntryCreateRoutes);
