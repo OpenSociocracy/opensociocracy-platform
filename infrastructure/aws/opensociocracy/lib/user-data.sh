f38c51a8f5b89b4b4308fc49feaefc45f494bd17
Brian Winkers <bwinkers@gmail.com>
2023-06-24 12:33:38 -0700

    Updated build instructions to match latest build.

--- infrastructure/aws/opensociocracy/lib/user-data.sh
+++ infrastructure/aws/opensociocracy/lib/user-data.sh
@@ -11,12 +11,20 @@ sudo apt install redis postgresql-client git curl  apt-get install \
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+ echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update 
 
-sudo usermod -aG docker admin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-USER admin
+sudo usermod -aG docker ubuntu
 
-cd /home/admin
+USER ubuntu
+
+cd /home/ubuntu
 
