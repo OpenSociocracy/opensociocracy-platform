29f3389d462141957ad36bd4de78b99211475992
Brian Winkers <bwinkers@gmail.com>
2023-06-24 20:02:10 -0700

    Site link cleanup

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     ],
 
     footer: {
-      message: 'Sponsored by <a href="https://coop.ultri.com/">Ultri</a>',
+      message: 'Sponsored by <a href="https://www.ultri.com/">Ultri</a>',
       copyright: 'Released under the <a href="https://github.com/OpenSociocracy/documentation-website/blob/main/LICENSE">MIT</a> and <a href="https://github.com/OpenSociocracy/open-sociocracy-saas/blob/main/LICENSE">AGPL</a> License</a>.<br />Copyright <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA</a>'
     }
   }
